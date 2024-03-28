e824e665d3bee45f80e8295d4d20c9f4c836c931
NakaylaRSHart <nakaylahart@DESKTOP-SJTLOE9>
2024-03-27 21:11:34 -0600

    Changed some styling/font sizes

--- src/PeopleUnorderedList.jsx
+++ src/PeopleUnorderedList.jsx
@@ -5,7 +5,7 @@ const PeopleUnorderedList = ({contactList}) => {
     <ul>
     {contactList.map(singleContact => (
       <li key={singleContact.id}>
-        <div>Name:</div>{singleContact.name} <div>Email:</div>{singleContact.email}, <div>Phone:</div>{singleContact.phone},<div>Address:</div> {singleContact.address.street}, {singleContact.address.suite}, {singleContact.city}, {singleContact.zipcode}.join(" ");
+        <div>Name:</div>{singleContact.name} <div>Email:</div>{singleContact.email}, <div>Phone:</div>{singleContact.phone},<div>Address:</div> {singleContact.address.street}, {singleContact.address.suite}, {singleContact.city}, {singleContact.zipcode};
       </li>
     ))}
   </ul>
